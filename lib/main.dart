a857e7ab17212a0d0891ca57d56378de47e3e4bc
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-08-08 22:19:37 +0530

    Removed redundant code

--- lib/main.dart
+++ lib/main.dart
@@ -51,101 +51,45 @@ class _MyHomePageState extends State<MyHomePage> {
           padding: EdgeInsets.all(10),
           child: TabBarView(
             children: [
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(18)),
-                      gradient: LinearGradient(
-                        colors: const [
-                          Color(0xff2c274c),
-                          Color(0xff46426c),
-                        ],
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const Text('Monthly Expenses', style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold, letterSpacing: 2), textAlign: TextAlign.center,),
-                        const SizedBox(height: 37),
-                        ConstrainedBox(
-                            constraints: BoxConstraints.expand(height: 200),
-                            child: Charts().lineChart()
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(18)),
-                      gradient: LinearGradient(
-                        colors: const [
-                          Color(0xff2c274c),
-                          Color(0xff46426c),
-                        ],
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const Text('Monthly Expenses', style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold, letterSpacing: 2), textAlign: TextAlign.center,),
-                        const SizedBox(height: 37),
-                        ConstrainedBox(
-                            constraints: BoxConstraints.expand(height: 200),
-                            child: Charts().pieChart(),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-//              Center(
-//                child: Charts().pieChart(),
-////                  child: Text('Pie', textAlign: TextAlign.center)
-//              ),
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(18)),
-                      gradient: LinearGradient(
-                        colors: const [
-                          Color(0xff2c274c),
-                          Color(0xff46426c),
-                        ],
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const Text('Monthly Expenses', style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold, letterSpacing: 2), textAlign: TextAlign.center,),
-                        const SizedBox(height: 37),
-                        ConstrainedBox(
-                          constraints: BoxConstraints.expand(height: 200),
-                          child: Charts().barChart(),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+              chartCards('Line Chart', Charts().lineChart()),
+              chartCards('Pie Chart', Charts().pieChart()),
+              chartCards('Bar Chart', Charts().barChart())
             ],
           ),
         ),
       )
+    );
+  }
+
+  Column chartCards(String title, Widget widget) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(18)),
+            gradient: LinearGradient(
+              colors: const [
+                Color(0xff2c274c),
+                Color(0xff46426c),
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(title, style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold, letterSpacing: 2), textAlign: TextAlign.center,),
+              const SizedBox(height: 37),
+              ConstrainedBox(
+                constraints: BoxConstraints.expand(height: 200),
+                child: widget,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
